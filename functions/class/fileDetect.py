b22e3c34deb9cbafb72ae95732f637de5d8eac8a
Creeper19472 <488476463@qq.com>
2020-04-27 22:31:47 +0800

    changes

--- functions/class/fileDetect.py
+++ functions/class/fileDetect.py
@@ -4,6 +4,15 @@ class Blocked:
     def ReplaceBlock(text, level):
         if text.find('<blocked>') == -1:
             return text
-        for i in :
-            blockstarts = text.find('<blocked>')
-            blockends = text.find('</blocked>', blockstarts)
+        blockpos = []
+        while True:
+            blockstarts = text.find('<blocked>', blockstarts) + 8
+            if blockstarts == -1:
+                break
+            blockends = text.find('</blocked>', blockstarts) + 9
+            if blockends == -1:
+                raise SyntaxError('Missing \'</blockends>\'')
+            blockpos.append((blockstarts, blockends))
+        return blockpos
+        
+print(Blocked.ReplaceBlock('<blocked>', 1))
